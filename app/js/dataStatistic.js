08033419fd6e81b112ddfeeacacfb20282fc10c3
ekamekas <mas.ekamekas@gmail.com>
2017-08-03 08:59:44 +0700

    Add grouping data

--- app/js/dataStatistic.js
+++ app/js/dataStatistic.js
@@ -1,77 +1,86 @@
 // Dummy data of network traffic in Mbps
-var threshold = 0.1   // +- 10% dari rerata
-var countLoop = 3     //
+var threshold = 0.1;   // +- 10% dari rerata
+var countLoop = 3;     //
 
 function DataStatistic(population){
-  this.population = population
+  this.population = population;
   this.sortedData = function(data){
     if(data === undefined)
-      data = population
+      data = population;
     function sortNumber(a,b) {
-        return a - b
+        return a - b;
     }
-    return data.sort(sortNumber)
-  }
+    return data.sort(sortNumber);
+  };
 
   this.sum = function(data){
     if(data === undefined)
-      data = population
-    let sum = 0
+      data = population;
+    let sum = 0;
     if(data.constructor === Array){
       for(i = 0; i < data.length; i++){
-        sum = sum + data[i]
+        sum = sum + data[i];
       }
     }
-    return sum
+    return sum;
   }
 
   this.mean = function(data){
     if(data === undefined)
-      data = population
-    return this.sum(data)/data.length
-  }
+      data = population;
+    return this.sum(data)/data.length;
+  };
 
   this.percentil = function(k, data){
     if(data === undefined)
-      data = population
-    let sortedData = this.sortedData(data)
-    return sortedData[Math.floor(k/100 * sortedData.length) - 1] + (k/100 % 1) * (sortedData[Math.floor(k/100 * sortedData.length)] - sortedData[Math.floor(k/100 * sortedData.length) - 1])
-  }
+      data = population;
+    let sortedData = this.sortedData(data);
+    return sortedData[Math.floor(k/100 * sortedData.length) - 1] + (k/100 % 1) * (sortedData[Math.floor(k/100 * sortedData.length)] - sortedData[Math.floor(k/100 * sortedData.length) - 1]);
+  };
 
   this.standardDeviation = function(data){
     if(data === undefined)
-      data = population
-    let diffSquare = []
-    let i = 0
+      data = population;
+    let diffSquare = [];
+    let i = 0;
     while(i < data.length) {
-      diffSquare.push(Math.pow(data[i] - this.mean(data), 2))
-      i++
-    }
-    return Math.sqrt(1/data.length * this.sum(diffSquare))
-  }
+      diffSquare.push(Math.pow(data[i] - this.mean(data), 2));
+      i++;
+    };
+    return Math.sqrt(1/data.length * this.sum(diffSquare));
+  };
 }
 
 // Mengelompokan data berdasarkan interval
 function DataCluster(population, radius){
-  let clusters = []
-  let group = []
-  let pivot = population[0]
+  this.findPeaks = function (array) {
+    let peaks = [];
+    for(let i = 0; i < array.length; i++){
+      if(array[i] > array[i + 1])
+        peaks.push(array[i]);
+    }
+    return peaks;
+  };
+
+  let clusters = [];
+  let group = [];
+  let pivot = population[0];
   this.sortedData = function(){
     function sortNumber(a,b) {
-        return a - b
+        return a - b;
     }
-    return population.sort(sortNumber)
-  }
+    return population.sort(sortNumber);
+  };
   for(i = 0; i < this.sortedData().length; i++){
     if(this.sortedData()[i] <= pivot + radius){
-      group.push(this.sortedData()[i])
+      group.push(this.sortedData()[i]);
     } else {
-      clusters.push(group)
-      group = []
-      pivot = this.sortedData()[i]
-      group.push(this.sortedData()[i])
+      clusters.push(group);
+      group = [];
+      pivot = this.sortedData()[i];
+      group.push(this.sortedData()[i]);
     }
   }
-    clusters.push(group)  // Group terakhirs
-  return clusters
+    clusters.push(group);  // Group terakhirs
+  return clusters;
 }
--- app/js/utility.js
+++ app/js/utility.js
@@ -3,38 +3,52 @@ function Utility(){
   https = require('https')
 
   this.getXML = function (url, callback) {
+
     'use strict';
     'use esversion:6'
 
     let xml2js = require('xml2js'),
     parser = new xml2js.Parser();
 
-    parser.on('error', function(err) { console.log('Parser error', err); });
+    // Check if parameter is file or http request
+    if(url.split('/')[0] == "file:"){
+      fs.readFile(__dirname + '/app/media/dataprtg/historicdata_may.xml', function(err, data) {
+          parser.parseString(data, function (err, result) {
+              console.log(result)
+              if(callback !== undefined)
+                callback(result)
+          });
+      });
+    } else if(url.split('/')[0] == "http:" || url.split('/')[0] == "https:"){
 
-    var request = require('request');
-    var agentOptions;
-    var agent;
-    agentOptions = {
-      host: '123.231.138.149',
-      port: '443',
-      path: '/',
-      rejectUnauthorized: false
-    };
-    agent = new https.Agent(agentOptions);
+      parser.on('error', function(err) { console.log('Parser error', err); });
 
-    request({
-      url: url,
-      method: 'GET',
-      agent: agent
-      }, function (err, resp, body) {
-      if (resp.statusCode >= 200 && resp.statusCode < 400) {
-          parser.parseString(body, function(err, result) {
-            console.log(result)
-            if(callback !== undefined)
-              callback(result)
-          })
+      var request = require('request');
+      var agentOptions;
+      var agent;
+
+      agentOptions = {
+        host: '123.231.138.149',
+        port: '443',
+        path: '/',
+        rejectUnauthorized: false
+      };
+      agent = new https.Agent(agentOptions);
+
+      request({
+        url: url,
+        method: 'GET',
+        agent: agent
+        }, function (err, resp, body) {
+        if (resp.statusCode >= 200 && resp.statusCode < 400) {
+            parser.parseString(body, function(err, result) {
+              console.log(result)
+              if(callback !== undefined)
+                callback(result)
+            })
+          }
         }
-      }
-    )
+      )
+    }
   }
 }
