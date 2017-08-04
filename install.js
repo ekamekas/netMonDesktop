16d724afc1a14cd4dead146f74cfb3ca56e961c5
ekamekas <mas.ekamekas@gmail.com>
2017-08-04 14:36:15 +0700

    Repair overwritting plot

---
+++ install.js
@@ -0,0 +1,9 @@
+var electronInstaller = require('electron-winstaller');
+resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: '../customPRTGDesktop',
+    outputDirectory: './installer64',
+    authors: 'My App Inc.',
+    exe: 'myapp.exe'
+  });
+
+resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
